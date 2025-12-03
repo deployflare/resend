26834e0c2c52d113e8ecc7b25684d58927b72873
multipleof4 <planetrenox@protonmail.com>
2025-12-03 14:20:24 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-async function fetch(request) {
-  if (request.method === 'OPTIONS') {
-    const headers = new Headers()
-    headers.set('Access-Control-Allow-Origin', '*')
-    headers.set('Access-Control-Allow-Headers', 'Authorization, Content-Type')
-    return new Response(null, { headers })
-  }
-  return new Response('OK')
-}
 
-export default { fetch }
